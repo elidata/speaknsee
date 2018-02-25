4e8bd0045f6ade5f5db2e79808e1cb1e842e526c
Eliedaat Adler <eliedaat@gmail.com>
2018-02-25 17:04:00 +0200

    supplement 404 error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+      return res.end("SPEAKNSEE APP 404 Not Found");
     }  
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
