b99ce31ff5d7aa1700a329025eaf81dff2dea653
Eliedaat Adler <eliedaat@gmail.com>
2018-03-12 17:02:49 +0200

    Update controller.js
    fix lang

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 	console.log('Starting SpeechRecognition library.');
-	var mylang = $('#mylang').val ;
+	var mylang = $('#mylang.val') ;
 	console.log('Language is'+ mylang) ;
 	var speech = new Speech(mylang);
 
